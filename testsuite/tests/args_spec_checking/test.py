7d0c2c24c04e883cab886a894aa1526db89ebe55
Raphaël AMIARD <raph.amiard@gmail.com>
2019-07-25 01:36:54 +0200

    Add args spec checking test

---
+++ testsuite/tests/args_spec_checking/test.py
@@ -0,0 +1,20 @@
+import gen
+
+p = gen.Shape()
+pos = gen.Point()
+
+def show_error(fn):
+    print "In show_error:"
+    try:
+        fn()
+    except TypeError as e:
+        print "    " + e.message
+    else:
+        print "    OK"
+
+
+show_error(lambda: p.translate())
+show_error(lambda: p.translate("lol"))
+show_error(lambda: p.translate(move=pos, pouet=pos))
+show_error(lambda: p.translate(pos))
+show_error(lambda: p.translate(move=pos))
