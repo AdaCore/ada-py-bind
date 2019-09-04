78129eb1331201c03979f34a7ffc0fe2db75fa59
Raphaël AMIARD <raph.amiard@gmail.com>
2019-09-04 12:38:53 +0200

    Remove lib dependent lines from test output

--- testsuite/build_lib.py
+++ testsuite/build_lib.py
@@ -6,6 +6,7 @@ the library and copying the resulting .so file to a name that python can find.
 import os.path as P
 from subprocess import Popen, PIPE
 import sys
+import re
 
 import e3.fs as fs
 
@@ -16,8 +17,16 @@ p = Popen([
 output, error = p.communicate()
 
 if p.returncode != 0:
-    # Build failed: Log the error and exit
+    # Build failed
+
+    # Remove some patterns from the error to make it resilient to changes in
+    # the library.
+    error = re.sub("^.*instantiation error at .*", "", error)
+
+    # Print the errors
     print error
+
+    # Exit
     sys.exit(1)
 else:
     # Rename libgen so that it's named according to python native modules
