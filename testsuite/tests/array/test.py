4a21c2c8b5535477530179a573ff80a1f297a440
Raphaël AMIARD <raph.amiard@gmail.com>
2019-12-09 21:47:43 +0100

    SC07-010: Add Py_Bind.Bounded_Array package
    New package to allow binding of bounded arrays to Python in an easy way.

---
+++ testsuite/tests/array/test.py
@@ -0,0 +1,15 @@
+import gen
+
+p = gen.Point()
+
+print p.coords[1]
+print p.coords[2]
+print p.coords[3]
+
+p.coords[1] = 12
+p.coords[2] = 13
+p.coords[3] = 14
+
+print p.coords[1]
+print p.coords[2]
+print p.coords[3]
